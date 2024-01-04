74ec2c613b0f957fde0f0af63c60fd8e8ea797a5
Sergei Blinov <blinovsv@gmail.com>
2024-01-04 17:18:28 +0100

    rename: tvm

---
+++ tvm_client/src/error.rs
@@ -0,0 +1,159 @@
+use crate::client::{binding_config, core_version};
+use crate::net;
+use chrono::TimeZone;
+use serde_json::Value;
+use std::fmt::Display;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Default, ApiType)]
+pub struct ClientError {
+    pub code: u32,
+    pub message: String,
+    pub data: Value,
+}
+
+pub type ClientResult<T> = Result<T, ClientError>;
+
+#[async_trait::async_trait]
+pub(crate) trait AddNetworkUrl: Sized {
+    async fn add_endpoint_from_context(
+        self,
+        context: &crate::ClientContext,
+        endpoint: &net::Endpoint,
+    ) -> Self {
+        if let Some(link) = &context.net.server_link {
+            self.add_endpoint(link, endpoint).await
+        } else {
+            self
+        }
+    }
+
+    async fn add_network_url(self, client: &net::ServerLink) -> Self {
+        self.add_network_url_from_state(client.state().as_ref())
+            .await
+    }
+
+    async fn add_network_url_from_context(self, client: &crate::ClientContext) -> Self {
+        if let Some(client) = &client.net.server_link {
+            self.add_network_url(client).await
+        } else {
+            self
+        }
+    }
+
+    async fn add_endpoint(self, link: &net::ServerLink, endpoint: &net::Endpoint) -> Self;
+
+    async fn add_network_url_from_state(self, state: &net::NetworkState) -> Self;
+}
+
+#[async_trait::async_trait]
+impl<T: Send> AddNetworkUrl for ClientResult<T> {
+    async fn add_endpoint(self, link: &net::ServerLink, endpoint: &net::Endpoint) -> Self {
+        match self {
+            Err(err) => Err(err.add_endpoint(link, endpoint).await),
+            _ => self,
+        }
+    }
+
+    async fn add_network_url_from_state(self, state: &net::NetworkState) -> Self {
+        match self {
+            Err(err) => Err(err.add_network_url_from_state(state).await),
+            _ => self,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl AddNetworkUrl for ClientError {
+    async fn add_endpoint(mut self, link: &net::ServerLink, endpoint: &net::Endpoint) -> Self {
+        self.data["config_servers"] = link.config_servers().await.into();
+        self.data["endpoint"] = Value::String(endpoint.query_url.clone());
+        self
+    }
+
+    async fn add_network_url_from_state(mut self, state: &net::NetworkState) -> Self {
+        self.data["config_servers"] = state.config_servers().await.into();
+        if let Some(endpoint) = state.query_endpoint().await {
+            self.data["query_url"] = endpoint.query_url.as_str().into();
+            if let Some(ip_address) = &endpoint.ip_address {
+                self.data["query_ip_address"] = ip_address.as_str().into();
+            }
+        }
+        self
+    }
+}
+
+impl Display for ClientError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if f.alternate() {
+            write!(f, "{:#}", json!(self))
+        } else {
+            write!(f, "{}", self.message)
+        }
+    }
+}
+
+impl std::error::Error for ClientError {}
+
+impl ClientError {
+    pub const CLIENT: isize = 0;
+    pub const CRYPTO: isize = 100;
+    pub const BOC: isize = 200;
+    pub const ABI: isize = 300;
+    pub const TVM: isize = 400;
+    pub const PROCESSING: isize = 500;
+    pub const NET: isize = 600;
+    pub const UTILS: isize = 700;
+    pub const DEBOT: isize = 800;
+
+    pub fn new(code: u32, message: String, data: Value) -> Self {
+        let mut data = data;
+        data["core_version"] = Value::String(core_version());
+        if let Some(binding) = binding_config() {
+            data["binding_library"] = Value::String(binding.library);
+            data["binding_version"] = Value::String(binding.version);
+        }
+        Self {
+            code,
+            message,
+            data,
+        }
+    }
+
+    pub fn with_code_message(code: u32, message: String) -> Self {
+        Self {
+            code,
+            message,
+            data: json!({
+                "core_version": core_version(),
+            }),
+        }
+    }
+
+    pub fn add_function(mut self, function: Option<&str>) -> ClientError {
+        if let Some(function) = function {
+            self.data["function_name"] = function.into();
+        }
+
+        self
+    }
+
+    pub fn add_address(mut self, address: &tvm_block::MsgAddressInt) -> ClientError {
+        self.data["account_address"] = address.to_string().into();
+        self
+    }
+
+    pub fn is_unauthorized(&self) -> bool {
+        self.code == net::ErrorCode::Unauthorized as u32
+    }
+}
+
+pub(crate) fn format_time(time: u32) -> String {
+    format!(
+        "{} ({})",
+        chrono::Local
+            .timestamp_opt(time as i64, 0)
+            .unwrap()
+            .to_rfc2822(),
+        time
+    )
+}
---
+++ tvm_client_processing/src/sdk_services/mod.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod mock_sdk_services;
+
+use crate::error;
+use crate::message_monitor::{MessageMonitoringParams, MessageMonitoringResult};
+#[cfg(test)]
+pub use mock_sdk_services::MockSdkServices;
+use std::future::Future;
+use tvm_types::Cell;
+
+pub struct NetSubscription(pub usize);
+
+#[async_trait]
+pub trait MessageMonitorSdkServices {
+    async fn subscribe_for_recent_ext_in_message_statuses<F: Future<Output = ()> + Send>(
+        &self,
+        messages: Vec<MessageMonitoringParams>,
+        callback: impl Fn(error::Result<Vec<MessageMonitoringResult>>) -> F + Send + Sync + 'static,
+    ) -> error::Result<NetSubscription>;
+
+    async fn unsubscribe(&self, subscription: NetSubscription) -> error::Result<()>;
+
+    fn spawn(&self, future: impl Future<Output = ()> + Send + 'static);
+
+    async fn sleep(&self, ms: u64) -> error::Result<()>;
+    fn now_ms(&self) -> u64;
+
+    fn cell_from_boc(&self, boc: &str, name: &str) -> error::Result<Cell>;
+}
