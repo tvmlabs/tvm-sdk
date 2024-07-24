77f4921246acd1fd277359ebbebac98036404c79
sehor05 <gorelushev.sergey@yandex.ru>
2024-07-24 22:47:52 +0300

    try again

--- tvm_vm/src/executor/token.rs
+++ tvm_vm/src/executor/token.rs
@@ -1,4 +1,4 @@
-use tvm_block::ACTION_ECC_MINT;
+use tvm_block::{ExtraCurrencyCollection, Serializable, VarUInteger32, ACTION_ECC_MINT};
 use tvm_types::BuilderData;
 
 use crate::executor::blockchain::add_action;
@@ -9,7 +9,12 @@ use crate::types::Status;
 
 pub(super) fn execute_ecc_mint(engine: &mut Engine) -> Status {
     engine.load_instruction(Instruction::new("MINT_TOKEN"))?;
-    fetch_stack(engine, 1)?;
-    let cell = engine.cmd.var(0).as_cell()?.clone();
-    add_action(engine, ACTION_ECC_MINT, Some(cell), BuilderData::new())
+    fetch_stack(engine, 2)?;
+    let x: u32 = engine.cmd.var(0).as_integer()?.into(0..=255)?;
+    let y: VarUInteger32 = VarUInteger32::from(engine.cmd.var(1).as_integer()?.into(0..=10000)?);
+    let mut data = ExtraCurrencyCollection::new();
+    data.set(&x, &y)?;
+    let mut cell = BuilderData::new();
+    data.write_to(&mut cell)?;
+    add_action(engine, ACTION_ECC_MINT, None, cell)
 }
