89fb21bfdfa0f4fb70bb26c4d76bac8823b85ac0
Alexander <alexander.s@gosh.sh>
2024-08-15 02:55:03 +0400

    fix fmt

--- tvm_debugger/src/decode.rs
+++ tvm_debugger/src/decode.rs
@@ -1,11 +1,25 @@
 use std::path::PathBuf;
 use std::str::FromStr;
+
 use tvm_abi::decode_function_response;
-use tvm_block::{CommonMsgInfo, CurrencyCollection, Deserializable, Grams, Message, MsgAddressInt, OutAction, OutActions, Serializable, StateInit};
-use tvm_types::{BuilderData, Cell, SliceData, write_boc};
+use tvm_block::CommonMsgInfo;
+use tvm_block::CurrencyCollection;
+use tvm_block::Deserializable;
+use tvm_block::Grams;
+use tvm_block::Message;
+use tvm_block::MsgAddressInt;
+use tvm_block::OutAction;
+use tvm_block::OutActions;
+use tvm_block::Serializable;
+use tvm_block::StateInit;
+use tvm_types::write_boc;
+use tvm_types::BuilderData;
+use tvm_types::Cell;
+use tvm_types::SliceData;
 use tvm_vm::stack::StackItem;
-use crate::Args;
+
 use crate::helper::load_abi_as_string;
+use crate::Args;
 
 pub(crate) fn decode_body(
     abi_file: &PathBuf,
@@ -13,32 +27,32 @@ pub(crate) fn decode_body(
     body: SliceData,
     internal: bool,
 ) -> anyhow::Result<()> {
-    let res = decode_function_response(
-        &load_abi_as_string(abi_file)?,
-        function,
-        body,
-        internal,
-        false
-    )
-        .map_err(|e| anyhow::format_err!(
-            "Failed to decode function response: {e}"
-        ))?;
+    let res =
+        decode_function_response(&load_abi_as_string(abi_file)?, function, body, internal, false)
+            .map_err(|e| anyhow::format_err!("Failed to decode function response: {e}"))?;
     println!("{res}");
     Ok(())
 }
 
-pub(crate) fn decode_actions(actions: StackItem, state: &mut StateInit, args: &Args) -> anyhow::Result<()> {
+pub(crate) fn decode_actions(
+    actions: StackItem,
+    state: &mut StateInit,
+    args: &Args,
+) -> anyhow::Result<()> {
     let abi_file = args.abi_file.as_ref();
     let function_name = args.function_name.as_ref();
-    let address = args.address.as_ref().map(|s| MsgAddressInt::from_str(s).unwrap()).unwrap_or_default();
+    let address =
+        args.address.as_ref().map(|s| MsgAddressInt::from_str(s).unwrap()).unwrap_or_default();
     if let StackItem::Cell(ref cell) = actions {
-        let actions: OutActions = OutActions::construct_from(&mut SliceData::load_cell(cell.clone())
-            .map_err(|e| anyhow::format_err!("{e}"))?).map_err(|e| anyhow::format_err!("{e}"))?;
+        let actions: OutActions = OutActions::construct_from(
+            &mut SliceData::load_cell(cell.clone()).map_err(|e| anyhow::format_err!("{e}"))?,
+        )
+        .map_err(|e| anyhow::format_err!("{e}"))?;
         println!("Output actions:\n----------------");
         let mut created_lt = 1;
         for act in actions {
             match act {
-                OutAction::SendMsg{mode: _, mut out_msg } => {
+                OutAction::SendMsg { mode: _, mut out_msg } => {
                     if out_msg.is_internal() {
                         out_msg.set_src_address(address.clone());
                         out_msg.set_at_and_lt(0, created_lt);
@@ -47,20 +61,25 @@ pub(crate) fn decode_actions(actions: StackItem, state: &mut StateInit, args: &A
                     println!("Action(SendMsg):\n{}", msg_printer(&out_msg)?);
                     if let Some(b) = out_msg.body() {
                         if abi_file.is_some() && function_name.is_some() {
-                            decode_body(abi_file.unwrap(), function_name.unwrap(), b, out_msg.is_internal())?;
+                            decode_body(
+                                abi_file.unwrap(),
+                                function_name.unwrap(),
+                                b,
+                                out_msg.is_internal(),
+                            )?;
                         }
                     }
-                },
-                OutAction::SetCode{ new_code: code } => {
+                }
+                OutAction::SetCode { new_code: code } => {
                     println!("Action(SetCode)");
                     state.code = Some(code);
-                },
+                }
                 OutAction::ReserveCurrency { .. } => {
                     println!("Action(ReserveCurrency)");
-                },
+                }
                 OutAction::ChangeLibrary { .. } => {
                     println!("Action(ChangeLibrary)");
-                },
+                }
                 _ => println!("Action(Unknown)"),
             };
         }
@@ -68,58 +87,71 @@ pub(crate) fn decode_actions(actions: StackItem, state: &mut StateInit, args: &A
     Ok(())
 }
 
-
 pub fn msg_printer(msg: &Message) -> anyhow::Result<String> {
     let mut b = BuilderData::new();
     msg.write_to(&mut b).map_err(|e| anyhow::format_err!("{e}"))?;
-    let bytes = write_boc(&b.into_cell().map_err(|e| anyhow::format_err!("{e}"))?).map_err(|e| anyhow::format_err!("{e}"))?;
-    Ok(format!("message header\n{}init  : {}\nbody  : {}\nbody_hex: {}\nbody_base64: {}\nboc_base64: {}\n",
-               print_msg_header(msg.header()),
-               msg.state_init().as_ref().map(|x| {
-                   state_init_printer(x)
-               }).unwrap_or_else(|| "None".to_string()),
-               match msg.body() {
-                   Some(slice) => format!("{:.2}", slice.into_cell()),
-                   None => "None".to_string(),
-               },
-               msg.body()
-                   .map(|b| hex::encode(&write_boc(&b.into_cell()).unwrap()))
-                   .unwrap_or_else(|| "None".to_string()),
-               tree_of_cells_into_base64(
-                   msg.body()
-                       .map(|slice| slice.into_cell())
-                       .as_ref(),
-               ),
-               base64::encode(bytes),
+    let bytes = write_boc(&b.into_cell().map_err(|e| anyhow::format_err!("{e}"))?)
+        .map_err(|e| anyhow::format_err!("{e}"))?;
+    Ok(format!(
+        "message header\n{}init  : {}\nbody  : {}\nbody_hex: {}\nbody_base64: {}\nboc_base64: {}\n",
+        print_msg_header(msg.header()),
+        msg.state_init()
+            .as_ref()
+            .map(|x| { state_init_printer(x) })
+            .unwrap_or_else(|| "None".to_string()),
+        match msg.body() {
+            Some(slice) => format!("{:.2}", slice.into_cell()),
+            None => "None".to_string(),
+        },
+        msg.body()
+            .map(|b| hex::encode(&write_boc(&b.into_cell()).unwrap()))
+            .unwrap_or_else(|| "None".to_string()),
+        tree_of_cells_into_base64(msg.body().map(|slice| slice.into_cell()).as_ref(),),
+        base64::encode(bytes),
     ))
 }
 
 pub fn state_init_printer(state: &StateInit) -> String {
     let serialized_data = state.write_to_bytes().unwrap();
-    format!("StateInit\n serialized_data: {}\n split_depth: {}\n special: {}\n data: {}\n code: {}\n code_hash: {}\n data_hash: {}\n code_depth: {}\n data_depth: {}\n version: {}\n lib:  {}\n",
-            hex::encode(&serialized_data),
-            state.split_depth.as_ref().map_or("None".to_string(), |x| x.as_u32().to_string()),
-            state.special.as_ref().map_or("None".to_string(), ToString::to_string),
-            tree_of_cells_into_base64(state.data.as_ref()),
-            tree_of_cells_into_base64(state.code.as_ref()),
-            state.code.as_ref().map(|code| code.repr_hash().to_hex_string()).unwrap_or_else(|| "None".to_string()),
-            state.data.as_ref().map(|code| code.repr_hash().to_hex_string()).unwrap_or_else(|| "None".to_string()),
-            state.code.as_ref().map(|code| code.repr_depth().to_string()).unwrap_or_else(|| "None".to_string()),
-            state.data.as_ref().map(|code| code.repr_depth().to_string()).unwrap_or_else(|| "None".to_string()),
-            get_version_mycode_aware(state.code.as_ref()).unwrap_or_else(|_| "None".to_string()),
-            tree_of_cells_into_base64(state.library.root()),
+    format!(
+        "StateInit\n serialized_data: {}\n split_depth: {}\n special: {}\n data: {}\n code: {}\n code_hash: {}\n data_hash: {}\n code_depth: {}\n data_depth: {}\n version: {}\n lib:  {}\n",
+        hex::encode(&serialized_data),
+        state.split_depth.as_ref().map_or("None".to_string(), |x| x.as_u32().to_string()),
+        state.special.as_ref().map_or("None".to_string(), ToString::to_string),
+        tree_of_cells_into_base64(state.data.as_ref()),
+        tree_of_cells_into_base64(state.code.as_ref()),
+        state
+            .code
+            .as_ref()
+            .map(|code| code.repr_hash().to_hex_string())
+            .unwrap_or_else(|| "None".to_string()),
+        state
+            .data
+            .as_ref()
+            .map(|code| code.repr_hash().to_hex_string())
+            .unwrap_or_else(|| "None".to_string()),
+        state
+            .code
+            .as_ref()
+            .map(|code| code.repr_depth().to_string())
+            .unwrap_or_else(|| "None".to_string()),
+        state
+            .data
+            .as_ref()
+            .map(|code| code.repr_depth().to_string())
+            .unwrap_or_else(|| "None".to_string()),
+        get_version_mycode_aware(state.code.as_ref()).unwrap_or_else(|_| "None".to_string()),
+        tree_of_cells_into_base64(state.library.root()),
     )
 }
 
 pub fn tree_of_cells_into_base64(root_cell: Option<&Cell>) -> String {
     match root_cell {
-        Some(cell) => {
-            match write_boc(cell) {
-                Ok(bytes) => base64::encode(bytes),
-                Err(_) => "None".to_string()
-            }
-        }
-        None => "None".to_string()
+        Some(cell) => match write_boc(cell) {
+            Ok(bytes) => base64::encode(bytes),
+            Err(_) => "None".to_string(),
+        },
+        None => "None".to_string(),
     }
 }
 
@@ -139,36 +171,41 @@ fn get_version(root: &Cell) -> anyhow::Result<String> {
     let cell2 = cell1.reference(1).map_err(|e| anyhow::format_err!("not found ({})", e))?;
     let bytes = cell2.data();
     match String::from_utf8(bytes.to_vec()) {
-        Ok(string) => if string.is_empty() { Ok("<empty>".to_string()) } else { Ok(string) },
-        Err(e) => Err(anyhow::format_err!("decoding failed ({})", e))
+        Ok(string) => {
+            if string.is_empty() {
+                Ok("<empty>".to_string())
+            } else {
+                Ok(string)
+            }
+        }
+        Err(e) => Err(anyhow::format_err!("decoding failed ({})", e)),
     }
 }
-
 
 fn print_msg_header(header: &CommonMsgInfo) -> String {
     match header {
         CommonMsgInfo::IntMsgInfo(header) => {
-            format!("   ihr_disabled: {}\n", header.ihr_disabled) +
-                &format!("   bounce      : {}\n", header.bounce) +
-                &format!("   bounced     : {}\n", header.bounced) +
-                &format!("   source      : {}\n", &header.src) +
-                &format!("   destination : {}\n", &header.dst) +
-                &format!("   value       : {}\n", print_cc(&header.value)) +
-                &format!("   ihr_fee     : {}\n", print_grams(&header.ihr_fee)) +
-                &format!("   fwd_fee     : {}\n", print_grams(&header.fwd_fee)) +
-                &format!("   created_lt  : {}\n", header.created_lt) +
-                &format!("   created_at  : {}\n", header.created_at)
-        },
+            format!("   ihr_disabled: {}\n", header.ihr_disabled)
+                + &format!("   bounce      : {}\n", header.bounce)
+                + &format!("   bounced     : {}\n", header.bounced)
+                + &format!("   source      : {}\n", &header.src)
+                + &format!("   destination : {}\n", &header.dst)
+                + &format!("   value       : {}\n", print_cc(&header.value))
+                + &format!("   ihr_fee     : {}\n", print_grams(&header.ihr_fee))
+                + &format!("   fwd_fee     : {}\n", print_grams(&header.fwd_fee))
+                + &format!("   created_lt  : {}\n", header.created_lt)
+                + &format!("   created_at  : {}\n", header.created_at)
+        }
         CommonMsgInfo::ExtInMsgInfo(header) => {
-            format!( "   source      : {}\n", &header.src) +
-                &format!("   destination : {}\n", &header.dst) +
-                &format!("   import_fee  : {}\n", print_grams(&header.import_fee))
-        },
+            format!("   source      : {}\n", &header.src)
+                + &format!("   destination : {}\n", &header.dst)
+                + &format!("   import_fee  : {}\n", print_grams(&header.import_fee))
+        }
         CommonMsgInfo::ExtOutMsgInfo(header) => {
-            format!( "   source      : {}\n", &header.src) +
-                &format!("   destination : {}\n", &header.dst) +
-                &format!("   created_lt  : {}\n", header.created_lt) +
-                &format!("   created_at  : {}\n", header.created_at)
+            format!("   source      : {}\n", &header.src)
+                + &format!("   destination : {}\n", &header.dst)
+                + &format!("   created_lt  : {}\n", header.created_lt)
+                + &format!("   created_at  : {}\n", header.created_at)
         }
     }
 }
@@ -181,10 +218,12 @@ fn print_cc(cc: &CurrencyCollection) -> String {
     let mut result = print_grams(&cc.grams);
     if !cc.other.is_empty() {
         result += " other: {";
-        cc.other.iterate_with_keys(|key: u32, value| {
-            result += &format!(" \"{}\": \"{}\",", key, value);
-            Ok(true)
-        }).ok();
+        cc.other
+            .iterate_with_keys(|key: u32, value| {
+                result += &format!(" \"{}\": \"{}\",", key, value);
+                Ok(true)
+            })
+            .ok();
         result.pop(); // remove extra comma
         result += " }";
     }
--- tvm_debugger/src/message.rs
+++ tvm_debugger/src/message.rs
@@ -1,22 +1,31 @@
-use std::str::FromStr;
-use tvm_abi::encode_function_call;
-use tvm_types::{BuilderData, ed25519_create_private_key, SliceData};
-use crate::Args;
-use crate::helper::{get_dest_address, get_now, load_abi_as_string, read_keys};
-
-#[cfg(test)]
-use std::str::FromStr;
 #[cfg(test)]
 use std::path::PathBuf;
+use std::str::FromStr;
+
 #[cfg(test)]
 use serde_json::json;
-
 use serde_json::Value;
-use tvm_block::{CurrencyCollection, ExternalInboundMessageHeader, ExtraCurrencyCollection, Grams, InternalMessageHeader, Message, MsgAddressExt, MsgAddressInt, VarUInteger32};
+use tvm_abi::encode_function_call;
+use tvm_block::CurrencyCollection;
+use tvm_block::ExternalInboundMessageHeader;
+use tvm_block::ExtraCurrencyCollection;
+use tvm_block::Grams;
+use tvm_block::InternalMessageHeader;
+use tvm_block::Message;
+use tvm_block::MsgAddressExt;
+use tvm_block::MsgAddressInt;
+use tvm_block::VarUInteger32;
+use tvm_types::ed25519_create_private_key;
+use tvm_types::SliceData;
 
+use crate::helper::get_dest_address;
+use crate::helper::get_now;
+use crate::helper::load_abi_as_string;
+use crate::helper::read_keys;
+use crate::Args;
 
 pub(crate) fn generate_message(args: &Args) -> anyhow::Result<(Message, SliceData)> {
-    let mut body = generate_message_body(args)?;
+    let body = generate_message_body(args)?;
     let message = if args.internal {
         generate_internal_message(args, Some(body.clone()))
     } else {
@@ -31,20 +40,16 @@ pub(crate) fn generate_external_message(
 ) -> anyhow::Result<Message> {
     let dst = get_dest_address(args)?;
     let header = ExternalInboundMessageHeader {
-        src: MsgAddressExt::with_extern(
-            SliceData::from_raw(vec![0x55; 8], 64)
-        ).unwrap(),
+        src: MsgAddressExt::with_extern(SliceData::from_raw(vec![0x55; 8], 64)).unwrap(),
         dst,
-        import_fee: 0x1234u64.into()  // Taken from TVM-linker
+        import_fee: 0x1234u64.into(), // Taken from TVM-linker
     };
     let mut msg = Message::with_ext_in_header(header);
     if let Some(body) = body {
         msg.set_body(body);
     }
     Ok(msg)
-
 }
-
 
 pub(crate) fn generate_internal_message(
     args: &Args,
@@ -54,7 +59,7 @@ pub(crate) fn generate_internal_message(
     let src = match args.message_source.as_ref() {
         Some(address) => MsgAddressInt::from_str(address)
             .map_err(|e| anyhow::format_err!("Failed to decode message source: {e}"))?,
-        None => MsgAddressInt::default()
+        None => MsgAddressInt::default(),
     };
     let grams = Grams::new(args.message_value.unwrap_or(0))
         .map_err(|e| anyhow::format_err!("Failed to setup message value: {e}"))?;
@@ -62,22 +67,17 @@ pub(crate) fn generate_internal_message(
     if let Some(value) = args.message_ecc.as_ref() {
         let map_value: Value = serde_json::from_str(value)
             .map_err(|e| anyhow::format_err!("Failed to decode message ecc: {e}"))?;
-        let ecc_map = map_value.as_object()
-            .ok_or(anyhow::format_err!("Failed to decode message ecc"))?;
+        let ecc_map =
+            map_value.as_object().ok_or(anyhow::format_err!("Failed to decode message ecc"))?;
         for (k, v) in ecc_map {
             let key = k.parse::<u32>()?;
-            let value_str = v.as_str()
-                .ok_or(anyhow::format_err!("Failed to parse ecc value"))?;
+            let value_str = v.as_str().ok_or(anyhow::format_err!("Failed to parse ecc value"))?;
             let value = VarUInteger32::from_str(value_str)
                 .map_err(|e| anyhow::format_err!("Failed to decode ecc value: {e}"))?;
-            ecc.set(&key, &value)
-                .map_err(|e| anyhow::format_err!("Failed to set ecc key: {e}"))?;
+            ecc.set(&key, &value).map_err(|e| anyhow::format_err!("Failed to set ecc key: {e}"))?;
         }
     }
-    let value = CurrencyCollection {
-        grams,
-        other: ecc
-    };
+    let value = CurrencyCollection { grams, other: ecc };
     let mut header = InternalMessageHeader::with_addresses(src, dst, value);
 
     // Constants taken from TVM-linker
@@ -97,14 +97,28 @@ pub(crate) fn generate_message_body(args: &Args) -> anyhow::Result<SliceData> {
     assert!(args.function_name.is_some());
     let abi = load_abi_as_string(args.abi_file.as_ref().unwrap())?;
     let function_name = args.function_name.as_ref().unwrap();
-    let header = args.abi_header.clone().map(|v| serde_json::to_string(&v).unwrap_or("{}".to_string()).trim_matches(|c| c == '"').replace('\\', "").to_string());
-    let parameters = args.call_parameters.clone().map(|v| serde_json::to_string(&v).unwrap().trim_matches(|c| c == '"').replace('\\', "").to_string()).unwrap_or("{}".to_string());
+    let header = args.abi_header.clone().map(|v| {
+        serde_json::to_string(&v)
+            .unwrap_or("{}".to_string())
+            .trim_matches(|c| c == '"')
+            .replace('\\', "")
+            .to_string()
+    });
+    let parameters = args
+        .call_parameters
+        .clone()
+        .map(|v| {
+            serde_json::to_string(&v)
+                .unwrap()
+                .trim_matches(|c| c == '"')
+                .replace('\\', "")
+                .to_string()
+        })
+        .unwrap_or("{}".to_string());
     let key = args.sign.as_ref().map(|path| {
         let keypair = read_keys(&path).expect("Failed to read key pair file");
-        let secret = hex::decode(&keypair.secret)
-            .expect("Failed to decode secret key");
-        ed25519_create_private_key(&secret)
-            .expect("Failed to load secret key")
+        let secret = hex::decode(&keypair.secret).expect("Failed to decode secret key");
+        ed25519_create_private_key(&secret).expect("Failed to load secret key")
     });
     let body = encode_function_call(
         &abi,
@@ -113,15 +127,15 @@ pub(crate) fn generate_message_body(args: &Args) -> anyhow::Result<SliceData> {
         &parameters,
         args.internal,
         key.as_ref(),
-        args.address.as_ref().map(|s| s.as_str())
-    ).map_err(|e| anyhow::format_err!("Failed to encode function call: {e}"))?;
+        args.address.as_ref().map(|s| s.as_str()),
+    )
+    .map_err(|e| anyhow::format_err!("Failed to encode function call: {e}"))?;
 
     let body = SliceData::load_builder(body)
         .map_err(|e| anyhow::format_err!("Failed to convert call body to slice data: {e}"))?;
 
     Ok(body)
 }
-
 
 #[test]
 fn test_encode_body() -> anyhow::Result<()> {
@@ -133,7 +147,8 @@ fn test_encode_body() -> anyhow::Result<()> {
     args.call_parameters = Some(json!({"index":35165}));
     let _body = generate_message_body(&args)?;
     args.internal = true;
-    args.address = Some("0:1111111111111111111111111111111111111111111111111111111111111111".to_string());
+    args.address =
+        Some("0:1111111111111111111111111111111111111111111111111111111111111111".to_string());
     let _body = generate_message_body(&args)?;
     Ok(())
 }
@@ -147,8 +162,10 @@ fn test_generate_message() -> anyhow::Result<()> {
     args.function_name = Some("iterate".to_string());
     args.call_parameters = Some(json!({"index":35165}));
     args.internal = true;
-    args.address = Some("0:1111111111111111111111111111111111111111111111111111111111111111".to_string());
-    args.message_source = Some("0:1111111111111111111111111111111111111111111111111111111111111122".to_string());
+    args.address =
+        Some("0:1111111111111111111111111111111111111111111111111111111111111111".to_string());
+    args.message_source =
+        Some("0:1111111111111111111111111111111111111111111111111111111111111122".to_string());
     let _int_message = generate_message(&args)?;
     Ok(())
 }
