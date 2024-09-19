deab446e103cc38b6de42eb001465b453b5cb4f3
sehor05 <gorelushev.sergey@yandex.ru>
2024-09-19 12:06:29 +0300

    u128 -> u64

--- tvm_vm/src/executor/token.rs
+++ tvm_vm/src/executor/token.rs
@@ -23,7 +23,7 @@ pub(super) fn execute_ecc_mint(engine: &mut Engine) -> Status {
     fetch_stack(engine, 2)?;
     let x: u32 = engine.cmd.var(0).as_integer()?.into(0..=255)?;
     let y: VarUInteger32 =
-        VarUInteger32::from(engine.cmd.var(1).as_integer()?.into(0..=u128::MAX)?);
+        VarUInteger32::from(engine.cmd.var(1).as_integer()?.into(0..=u64::MAX)?);
     let mut data = ExtraCurrencyCollection::new();
     data.set(&x, &y)?;
     let mut cell = BuilderData::new();
