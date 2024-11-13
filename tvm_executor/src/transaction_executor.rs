9ca911d40cfe2015cbbcfdb2d5fe010cd121e3f0
sehor05 <gorelushev.sergey@yandex.ru>
2024-11-13 14:40:30 +0300

    update

--- tvm_executor/src/transaction_executor.rs
+++ tvm_executor/src/transaction_executor.rs
@@ -1527,8 +1527,11 @@ fn outmsg_action_handler(
                     log::error!(target: "executor", "cannot sub grams : {}", err);
                     RESULT_CODE_UNSUPPORTED
                 })? {
-                    result_value.grams = Grams::zero();
-                    return Err(skip.map(|_| RESULT_CODE_NOT_ENOUGH_GRAMS).unwrap_or_default());
+                    match result_value.grams.sub(&reserved_value.grams) {
+                        Ok(false) => return Err(skip.map(|_| RESULT_CODE_NOT_ENOUGH_GRAMS).unwrap_or_default()),
+                        Ok(true) => {},
+                        Err(_) => return Err(skip.map(|_| RESULT_CODE_UNSUPPORTED).unwrap_or_default())
+                    };
                 }
             }
             int_header.value = result_value.clone();
@@ -1539,11 +1542,6 @@ fn outmsg_action_handler(
             // send all remainig balance of inbound message
             result_value.add(msg_balance).ok();
             if (mode & SENDMSG_PAY_FEE_SEPARATELY) == 0 {
-                log::trace!(
-                    target: "executor",
-                    "msg balance {} is too small, cannot pay phase fees: {}",
-                    result_value.grams, compute_phase_fees
-                );
                 if &result_value.grams < compute_phase_fees {
                     return Err(skip.map(|_| RESULT_CODE_NOT_ENOUGH_GRAMS).unwrap_or_default());
                 }
@@ -1559,7 +1557,7 @@ fn outmsg_action_handler(
             result_value.grams += total_fwd_fees;
         } else if int_header.value.grams < total_fwd_fees {
             // msg value is too small, reciever cannot pay the fees
-            log::trace!(
+            log::warn!(
                 target: "executor",
                 "msg balance {} is too small, cannot pay fwd+ihr fees: {}",
                 int_header.value.grams, total_fwd_fees
@@ -1581,7 +1579,7 @@ fn outmsg_action_handler(
     }
 
     if acc_balance.grams < result_value.grams {
-        log::trace!(
+        log::warn!(
             target: "executor",
             "account balance {} is too small, cannot send {}", acc_balance.grams, result_value.grams
         );
@@ -1589,7 +1587,7 @@ fn outmsg_action_handler(
     }
     match acc_balance.sub(&result_value) {
         Ok(false) | Err(_) => {
-            log::trace!(
+            log::warn!(
                 target: "executor",
                 "account balance {} is too small, cannot send {}", acc_balance, result_value
             );
@@ -1606,7 +1604,7 @@ fn outmsg_action_handler(
     };
     match acc_balance.other.iterate_with_keys(predicate) {
         Ok(false) | Err(_) => {
-            log::trace!(target: "executor", "Cannot reduce account extra balance");
+            log::warn!(target: "executor", "Cannot reduce account extra balance");
             return Err(skip.map(|_| RESULT_CODE_INVALID_BALANCE).unwrap_or_default());
         }
         _ => (),
