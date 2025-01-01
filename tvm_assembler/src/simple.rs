ab09492f00a188c5926bf5ac86516784b7c15f7a
sehor05 <gorelushev.sergey@yandex.ru>
2025-01-01 18:39:03 +0300

    add reward adjustment

--- tvm_assembler/src/simple.rs
+++ tvm_assembler/src/simple.rs
@@ -843,7 +843,8 @@ impl Engine {
         CALCBKREWARD                         => 0xC7, 0x29
         CALCMINSTAKE                         => 0xC7, 0x30
         VERGRTH16                            => 0xC7, 0x31
-        POSEIDON                     => 0xC7, 0x32
+        POSEIDON                             => 0xC7, 0x32
+        CALCBKREWARDADJ                      => 0xC7, 0x33
     }
 
     #[cfg(feature = "groth")]
