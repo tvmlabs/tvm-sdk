3fbad1caaddc46c64abbb162b1dbdcca2eacd7ad
Gorelyshev Sergey <49919055+SeHor05@users.noreply.github.com>
2025-02-19 12:35:36 +0100

    fmt

--- tvm_api/src/ton/control/config/local.rs
+++ tvm_api/src/ton/control/config/local.rs
@@ -1,1 +1,40 @@
-use serde_derive :: { Serialize , Deserialize } ;# [derive (Debug , Default , Clone , PartialEq)] # [doc = "TL-derived from `control.config.local`\n\n```text\ncontrol.config.local priv:PrivateKey pub:int256 port:int = control.config.Local;\n```\n"] pub struct Local { pub priv_ : crate :: ton :: PrivateKey , pub pub_ : crate :: ton :: int256 , pub port : crate :: ton :: int , } impl Eq for Local { } impl crate :: BareSerialize for Local { fn constructor (& self) -> crate :: ConstructorNumber { crate :: ConstructorNumber (0x751deced) } fn serialize_bare (& self , _ser : & mut crate :: Serializer) -> crate :: Result < () > { let Local { priv_ , pub_ , port } = self ; _ser . write_boxed :: < crate :: ton :: PrivateKey > (priv_) ? ; _ser . write_bare :: < crate :: ton :: int256 > (pub_) ? ; _ser . write_bare :: < crate :: ton :: int > (port) ? ; Ok (()) } } impl crate :: BareDeserialize for Local { fn deserialize_bare (_de : & mut crate :: Deserializer) -> crate :: Result < Self > { { let priv_ = _de . read_boxed :: < crate :: ton :: PrivateKey > () ? ; let pub_ = _de . read_bare :: < crate :: ton :: int256 > () ? ; let port = _de . read_bare :: < crate :: ton :: int > () ? ; Ok (Self { priv_ , pub_ , port , }) } } } impl crate :: IntoBoxed for Local { type Boxed = crate :: ton :: control :: config :: Local ; fn into_boxed (self) -> crate :: ton :: control :: config :: Local { crate :: ton :: control :: config :: Local :: Control_Config_Local (self) } }
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+#[derive(Debug, Default, Clone, PartialEq)]
+#[doc = "TL-derived from `control.config.local`\n\n```text\ncontrol.config.local priv:PrivateKey pub:int256 port:int = control.config.Local;\n```\n"]
+pub struct Local {
+    pub priv_: crate::ton::PrivateKey,
+    pub pub_: crate::ton::int256,
+    pub port: crate::ton::int,
+}
+impl Eq for Local {}
+impl crate::BareSerialize for Local {
+    fn constructor(&self) -> crate::ConstructorNumber {
+        crate::ConstructorNumber(0x751deced)
+    }
+
+    fn serialize_bare(&self, _ser: &mut crate::Serializer) -> crate::Result<()> {
+        let Local { priv_, pub_, port } = self;
+        _ser.write_boxed::<crate::ton::PrivateKey>(priv_)?;
+        _ser.write_bare::<crate::ton::int256>(pub_)?;
+        _ser.write_bare::<crate::ton::int>(port)?;
+        Ok(())
+    }
+}
+impl crate::BareDeserialize for Local {
+    fn deserialize_bare(_de: &mut crate::Deserializer) -> crate::Result<Self> {
+        {
+            let priv_ = _de.read_boxed::<crate::ton::PrivateKey>()?;
+            let pub_ = _de.read_bare::<crate::ton::int256>()?;
+            let port = _de.read_bare::<crate::ton::int>()?;
+            Ok(Self { priv_, pub_, port })
+        }
+    }
+}
+impl crate::IntoBoxed for Local {
+    type Boxed = crate::ton::control::config::Local;
+
+    fn into_boxed(self) -> crate::ton::control::config::Local {
+        crate::ton::control::config::Local::Control_Config_Local(self)
+    }
+}
--- tvm_api/src/ton/pchan/promise.rs
+++ tvm_api/src/ton/pchan/promise.rs
@@ -1,1 +1,48 @@
-use serde_derive :: { Serialize , Deserialize } ;# [derive (Debug , Default , Clone , PartialEq)] # [doc = "TL-derived from `pchan.promise`\n\n```text\npchan.promise signature:bytes promise_A:int64 promise_B:int64 channel_id:int64 = pchan.Promise;\n```\n"] pub struct Promise { pub signature : crate :: ton :: bytes , pub promise_A : crate :: ton :: int64 , pub promise_B : crate :: ton :: int64 , pub channel_id : crate :: ton :: int64 , } impl Eq for Promise { } impl crate :: Signing for Promise { fn signature_mut (& mut self) -> & mut crate :: ton :: bytes { & mut self . signature } } impl crate :: BareSerialize for Promise { fn constructor (& self) -> crate :: ConstructorNumber { crate :: ConstructorNumber (0xa20e945d) } fn serialize_bare (& self , _ser : & mut crate :: Serializer) -> crate :: Result < () > { let Promise { signature , promise_A , promise_B , channel_id } = self ; _ser . write_bare :: < crate :: ton :: bytes > (signature) ? ; _ser . write_bare :: < crate :: ton :: int64 > (promise_A) ? ; _ser . write_bare :: < crate :: ton :: int64 > (promise_B) ? ; _ser . write_bare :: < crate :: ton :: int64 > (channel_id) ? ; Ok (()) } } impl crate :: BareDeserialize for Promise { fn deserialize_bare (_de : & mut crate :: Deserializer) -> crate :: Result < Self > { { let signature = _de . read_bare :: < crate :: ton :: bytes > () ? ; let promise_A = _de . read_bare :: < crate :: ton :: int64 > () ? ; let promise_B = _de . read_bare :: < crate :: ton :: int64 > () ? ; let channel_id = _de . read_bare :: < crate :: ton :: int64 > () ? ; Ok (Self { signature , promise_A , promise_B , channel_id , }) } } } impl crate :: IntoBoxed for Promise { type Boxed = crate :: ton :: pchan :: Promise ; fn into_boxed (self) -> crate :: ton :: pchan :: Promise { crate :: ton :: pchan :: Promise :: Pchan_Promise (self) } }
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+#[derive(Debug, Default, Clone, PartialEq)]
+#[doc = "TL-derived from `pchan.promise`\n\n```text\npchan.promise signature:bytes promise_A:int64 promise_B:int64 channel_id:int64 = pchan.Promise;\n```\n"]
+pub struct Promise {
+    pub signature: crate::ton::bytes,
+    pub promise_A: crate::ton::int64,
+    pub promise_B: crate::ton::int64,
+    pub channel_id: crate::ton::int64,
+}
+impl Eq for Promise {}
+impl crate::Signing for Promise {
+    fn signature_mut(&mut self) -> &mut crate::ton::bytes {
+        &mut self.signature
+    }
+}
+impl crate::BareSerialize for Promise {
+    fn constructor(&self) -> crate::ConstructorNumber {
+        crate::ConstructorNumber(0xa20e945d)
+    }
+
+    fn serialize_bare(&self, _ser: &mut crate::Serializer) -> crate::Result<()> {
+        let Promise { signature, promise_A, promise_B, channel_id } = self;
+        _ser.write_bare::<crate::ton::bytes>(signature)?;
+        _ser.write_bare::<crate::ton::int64>(promise_A)?;
+        _ser.write_bare::<crate::ton::int64>(promise_B)?;
+        _ser.write_bare::<crate::ton::int64>(channel_id)?;
+        Ok(())
+    }
+}
+impl crate::BareDeserialize for Promise {
+    fn deserialize_bare(_de: &mut crate::Deserializer) -> crate::Result<Self> {
+        {
+            let signature = _de.read_bare::<crate::ton::bytes>()?;
+            let promise_A = _de.read_bare::<crate::ton::int64>()?;
+            let promise_B = _de.read_bare::<crate::ton::int64>()?;
+            let channel_id = _de.read_bare::<crate::ton::int64>()?;
+            Ok(Self { signature, promise_A, promise_B, channel_id })
+        }
+    }
+}
+impl crate::IntoBoxed for Promise {
+    type Boxed = crate::ton::pchan::Promise;
+
+    fn into_boxed(self) -> crate::ton::pchan::Promise {
+        crate::ton::pchan::Promise::Pchan_Promise(self)
+    }
+}
