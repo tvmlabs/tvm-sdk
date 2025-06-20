c16a9a33a89a84e005e327be39bfea6c1352230d
ignaty8 <ignaty8@users.noreply.github.com>
2025-06-20 09:56:53 +0200

    Fix wasm cargo tests (#103)

--- examples/wasm/rust/calc_pi/src/lib.rs
+++ examples/wasm/rust/calc_pi/src/lib.rs
@@ -5,21 +5,21 @@ mod bindings;
 // The comments that follow the `use` declaration below
 // correlate the rust module path segments with their
 // `world.wit` counterparts:
-use bindings::exports::docs::adder::add_interface::Guest;
+use bindings::exports::docs::calc_pi::calc_interface::Guest;
 //            <- items bundled with `export` keyword
 //                     <- package namespace
 //                           <- package
 //                                  <- interface name
+mod calc;
 
 struct Component;
 
 impl Guest for Component {
     fn add(kwargs: Vec<u8>) -> Vec<u8> {
-        [kwargs[0] + kwargs[1]].to_vec()
-    }
-
-    fn substract(kwargs: Vec<u8>) -> Vec<u8> {
-        [kwargs[0] - kwargs[1]].to_vec()
+        // let digits: u64 = (kwargs[0]) << 2 + kwargs[1];
+        let number = u64::from_be_bytes([0, 0, 0, 0, 0, 0, kwargs[0], kwargs[1]]);
+        calc::calc::pi(number).as_bytes().to_vec()
+        //[kwargs[0] + kwargs[1]].to_vec()
     }
 }
 
