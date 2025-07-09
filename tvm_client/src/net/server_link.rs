1a4adb1fd07d568e875da7738b239cf016214c62
Artem Zhdanov <artem.a.zhdanov@gmail.com>
2025-07-09 16:40:17 +0500

    One REST API endpoint for accounts and messages

--- tvm_client/src/net/server_link.rs
+++ tvm_client/src/net/server_link.rs
@@ -58,7 +58,10 @@ pub const MAX_TIMEOUT: u32 = i32::MAX as u32;
 pub const MIN_RESUME_TIMEOUT: u32 = 500;
 pub const MAX_RESUME_TIMEOUT: u32 = 3000;
 pub const ENDPOINT_CACHE_TIMEOUT: u64 = 10 * 60 * 1000;
+
+pub const API_VERSION: &str = "v2";
 pub const REST_API_PORT: u16 = 8600;
+pub const ENDPOINT_MESSAGES: &str = "messages";
 
 pub(crate) struct Subscription {
     pub unsubscribe: Pin<Box<dyn Future<Output = ()> + Send>>,
@@ -93,7 +96,6 @@ pub(crate) struct NetworkState {
     resume_timeout: AtomicU32,
     query_endpoint: RwLock<Option<Arc<Endpoint>>>,
     resolved_endpoints: RwLock<HashMap<String, ResolvedEndpoint>>,
-    bm_send_message_endpoint: RwLock<String>,
     bk_send_message_endpoint: RwLock<Option<String>>,
     rest_api_endpoint: RwLock<Url>,
     bm_license_contract: RwLock<Option<String>>,
@@ -118,7 +120,6 @@ impl NetworkState {
         client_env: Arc<ClientEnv>,
         config: NetworkConfig,
         endpoint_addresses: Vec<String>,
-        bm_send_message_endpoint: String,
         rest_api_endpoint: Url,
     ) -> Self {
         let (sender, receiver) = watch::channel(false);
@@ -136,7 +137,6 @@ impl NetworkState {
             resume_timeout: AtomicU32::new(0),
             query_endpoint: RwLock::new(None),
             resolved_endpoints: Default::default(),
-            bm_send_message_endpoint: RwLock::new(bm_send_message_endpoint),
             bk_send_message_endpoint: RwLock::new(None),
             rest_api_endpoint: RwLock::new(rest_api_endpoint),
             bm_license_contract: RwLock::new(None),
@@ -374,13 +374,8 @@ impl NetworkState {
         if let Some(bk_endpoint) = guarded_bk_endpoint.as_ref() {
             bk_endpoint.clone()
         } else {
-            let bm_endpoint = self.bm_send_message_endpoint.read().await;
-            bm_endpoint.clone()
+            self.rest_api_endpoint.read().await.clone().to_string()
         }
-    }
-
-    pub async fn get_bm_send_message_endpoint(&self) -> String {
-        self.bm_send_message_endpoint.read().await.to_string()
     }
 
     pub async fn get_rest_api_endpoint(&self) -> Url {
@@ -456,43 +451,36 @@ fn replace_endpoints(endpoints: Vec<String>) -> Vec<String> {
     result
 }
 
-fn construct_rest_api_endpoint(original: &str, use_https: bool) -> ClientResult<Url> {
-    let original = if original.contains("://") {
-        original.to_string()
-    } else {
-        format!("http://{}", original)
-    };
-    let mut rest_api_endpoint = Url::parse(&original).map_err(Error::parse_url_failed)?;
-
-    let scheme = if use_https { "https" } else { "http" };
-    rest_api_endpoint
-        .set_scheme(scheme)
-        .map_err(|_| Error::modify_url_failed("Failed to set scheme"))?;
-
-    rest_api_endpoint
-        .set_port(Some(REST_API_PORT))
-        .map_err(|_| Error::modify_url_failed("Failed to set port for REST API endpoint"))?;
-    Ok(rest_api_endpoint)
-}
-
-fn construct_bm_send_message_endpoint(original: &str, use_https: bool) -> ClientResult<String> {
+fn construct_rest_api_endpoint(original: &str) -> ClientResult<Url> {
+    // Set HTTP if scheme is not presented
     let original = if original.contains("://") {
         original.to_string()
     } else {
         format!("http://{}", original)
     };
 
-    let mut url = reqwest::Url::parse(&original).map_err(Error::parse_url_failed)?;
+    let url = Url::parse(&original).map_err(Error::parse_url_failed)?;
+    let scheme = url.scheme();
+    let host = url.host().ok_or_else(|| Error::parse_url_failed("Missing host in URL"))?;
+    let mut port = url
+        .port_or_known_default()
+        .ok_or_else(|| Error::parse_url_failed("Missing port in URL"))?;
 
-    let scheme = if use_https { "https" } else { "http" };
-    url.set_scheme(scheme).map_err(|_| Error::modify_url_failed("Failed to set scheme"))?;
-    url.set_port(Some(8700)).map_err(|_| Error::modify_url_failed("Failed to set port"))?;
-    url.set_path("/bm/v2/messages");
+    // Set the port for the REST API if no specific port is specified
+    if (scheme == "http" && port == 80) || (scheme == "https" && port == 443) {
+        port = REST_API_PORT
+    }
+    // Reconstruct the URL from scheme, host, and port.
+    // This way we remove any /path/something part from url if it is presented
+    let url_str = format!("{scheme}://{host}:{port}");
+    let mut url = Url::parse(&url_str).map_err(Error::parse_url_failed)?;
 
-    Ok(url.to_string())
+    url.set_path(&format!("{API_VERSION}/"));
+    Ok(url)
 }
 
-fn get_redirection_data(data: &Value) -> (Option<String>, Option<String>) {
+fn get_redirection_data(data: &Value) -> (Option<String>, Option<Result<Url, ClientError>>) {
+    // TODO: Add type RedirectionData
     let producers_opt = data.get("result").and_then(|res| res.get("producers")).or_else(|| {
         data.get("node_error")
             .and_then(|ne| ne.get("extensions"))
@@ -504,13 +492,7 @@ fn get_redirection_data(data: &Value) -> (Option<String>, Option<String>) {
         .and_then(|val| val.as_array())
         .and_then(|arr| arr.first())
         .and_then(|v| v.as_str())
-        .map(|s| {
-            if s.contains(':') {
-                format!("http://{}/bk/v2/messages", s)
-            } else {
-                format!("http://{}:8600/bk/v2/messages", s)
-            }
-        });
+        .map(|s| construct_rest_api_endpoint(s));
 
     let thread_id = data
         .get("node_error")
@@ -529,17 +511,16 @@ impl ServerLink {
         if endpoint_addresses.is_empty() {
             return Err(crate::client::Error::net_module_not_init());
         }
-        let endpoint_addresses = replace_endpoints(endpoint_addresses);
-        let bm_send_message_endpoint =
-            construct_bm_send_message_endpoint(&endpoint_addresses[0], false)?;
+        let rest_api_addr = endpoint_addresses[0].clone();
 
-        let rest_api_endpoint = construct_rest_api_endpoint(&endpoint_addresses[0], false)?;
+        let endpoint_addresses = replace_endpoints(endpoint_addresses);
+
+        let rest_api_endpoint = construct_rest_api_endpoint(&rest_api_addr)?;
 
         let state = Arc::new(NetworkState::new(
             client_env.clone(),
             config.clone(),
             endpoint_addresses,
-            bm_send_message_endpoint,
             rest_api_endpoint,
         ));
 
@@ -962,6 +943,11 @@ impl ServerLink {
         msg_body: &[u8],
         thread_id: ThreadIdentifier,
     ) -> ClientResult<Value> {
+        // This function adds "resource" part to the REST path
+        fn ensure_resource(path: &str) -> String {
+            let resource = ENDPOINT_MESSAGES;
+            if path.ends_with(resource) { path.to_string() } else { format!("{path}/{resource}") }
+        }
         let mut attempts = 0;
 
         let network_state = self.state();
@@ -975,14 +961,24 @@ impl ServerLink {
         };
 
         let mut endpoint = network_state.select_send_message_endpoint().await;
+
         let query = json!([message]).to_string();
-        let mut result = self.query_http(query, &endpoint).await;
+        let mut result = self.query_http(query, &ensure_resource(&endpoint)).await;
         while attempts < self.config.message_retries_count {
             attempts += 1;
             if let Ok(ref data) = result {
-                let (_, bk_url) = get_redirection_data(data);
-                if bk_url.is_some() {
-                    network_state.update_bk_send_message_endpoint(bk_url).await;
+                let (_, res) = get_redirection_data(data);
+                if let Some(res) = res {
+                    match res {
+                        Ok(bk_url) => {
+                            network_state
+                                .update_bk_send_message_endpoint(Some(bk_url.to_string()))
+                                .await;
+                        }
+                        Err(err) => {
+                            log::error!("{err}")
+                        }
+                    }
                 }
                 if let Some(bm_data) = data.get("block_manager") {
                     network_state.update_bm_data(bm_data).await;
@@ -1010,22 +1006,33 @@ impl ServerLink {
             }
 
             if code == "TOKEN_EXPIRED" {
-                endpoint = network_state.get_bm_send_message_endpoint().await;
+                endpoint = network_state.get_rest_api_endpoint().await.clone().to_string(); // ZZZ
                 network_state.update_bk_send_message_endpoint(None).await;
             }
 
-            let (real_thread_id, redirect_url) = get_redirection_data(&err.data);
+            let (real_thread_id, res) = get_redirection_data(&err.data);
 
             if let Some(thread_id) = real_thread_id {
                 message.set_thread_id(Some(thread_id));
             }
 
-            if let Some(ref bk_endpoint) = redirect_url {
-                endpoint = bk_endpoint.to_string();
-                network_state.update_bk_send_message_endpoint(redirect_url).await;
+            if let Some(res) = res {
+                match res {
+                    Ok(bk_url) => {
+                        network_state
+                            .update_bk_send_message_endpoint(Some(bk_url.to_string()))
+                            .await;
+                    }
+                    Err(err) => {
+                        log::error!("{err}")
+                    }
+                }
             }
 
-            result = self.query_http(json!([message]).to_string(), &endpoint).await;
+            // We leave `host`,`port` and all parts of the path untouched, except that we
+            // ensure that the path always ends with "ENDPOINT_MESSAGES"
+            result =
+                self.query_http(json!([message]).to_string(), &ensure_resource(&endpoint)).await;
         }
 
         result
