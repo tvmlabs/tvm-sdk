7853ba5d0f02da8c9c3a4f58c06e84ea9b17ec6f
alinaT <alina.t@gosh.sh>
2025-07-23 14:53:48 +0300

    rsa mod pow tests

--- tvm_vm/benches/benchmarks.rs
+++ tvm_vm/benches/benchmarks.rs
@@ -28,6 +28,11 @@ use base64ct::Encoding;
 
 use std::time::Duration;
 
+use num_bigint::BigInt;
+use num_bigint::BigUint;
+use std::str::FromStr;
+use num_traits::FromPrimitive;
+
 use criterion::Criterion;
 use criterion::SamplingMode;
 use criterion::criterion_group;
@@ -804,6 +809,104 @@ fn bench_wasmtls(c: &mut Criterion) {
     });
 }
 
+
+// Run  `cargo bench -p tvm_vm --bench benchmarks wasmcalc`
+fn bench_wasmcalc(c: &mut Criterion) {
+    c.bench_function("wasmcalc", |b| {
+        let mut total_duration = Duration::default();
+        b.iter_custom(|iters| {
+            // b.iter(|| {
+            //+++++ Let's say I want to measure execution time from this point.
+
+            for _i in 0..iters {
+                let mut stack = Stack::new();
+                let hash_str = "039f258266a19e9463f4235d9467b5b02f0ad5872cda065afdb14066a8bd5f4f";
+                let hash: Vec<u8> = (0..hash_str.len())
+                    .step_by(2)
+                    .map(|i| u8::from_str_radix(&hash_str[i..i + 2], 16).unwrap())
+                    .collect::<Vec<u8>>();
+                let cell =
+                        TokenValue::write_bytes(hash.as_slice(), &ABI_VERSION_2_4).unwrap().into_cell().unwrap();
+                stack.push(StackItem::cell(cell.clone()));
+                let cell = TokenValue::write_bytes(&[0u8, 0u8, 0u8, 0u8], &ABI_VERSION_2_4).unwrap().into_cell().unwrap();
+                stack.push(StackItem::cell(cell.clone()));
+                // Push args, func name, instance name, then wasm.
+                let wasm_func = "add";
+                let cell = pack_data_to_cell(&wasm_func.as_bytes(), &mut 0).unwrap();
+                stack.push(StackItem::cell(cell.clone()));
+                let wasm_func = "docs:calc-pi/calc-interface@0.1.0";
+                let cell = pack_data_to_cell(&wasm_func.as_bytes(), &mut 0).unwrap();
+                stack.push(StackItem::cell(cell.clone()));
+                let wasm_dict = Vec::<u8>::new();
+
+                let cell = TokenValue::write_bytes(&wasm_dict.as_slice(), &ABI_VERSION_2_4)
+                    .unwrap()
+                    .into_cell()
+                    .unwrap();
+                // let cell = split_to_chain_of_cells(wasm_dict);
+                // let cell = pack_data_to_cell(&wasm_dict, &mut engine).unwrap();
+                stack.push(StackItem::cell(cell.clone()));
+
+                let mut res = Vec::<u8>::with_capacity(3);
+                res.push(0xC7);
+                //res.push(0x3A);
+                res.push(0x39);
+                res.push(0x80);
+    
+                let code = SliceData::new(res);
+
+                let mut engine = Engine::with_capabilities(0).setup_with_libraries(code, None, Some(stack), None, vec![]);
+
+               
+                let start = std::time::Instant::now();
+                engine.execute();
+                total_duration += start.elapsed();
+                
+            }
+            total_duration
+
+            // println!("ress: {:?}", hex::encode(ress));
+        })
+    });
+}
+
+
+// Run  `cargo bench -p tvm_vm --bench benchmarks rsamod`
+fn bench_rsamod(c: &mut Criterion) {
+    c.bench_function("rsamod", |b| {
+        let mut total_duration = Duration::default();
+        b.iter_custom(|iters| {
+            // b.iter(|| {
+            //+++++ Let's say I want to measure execution time from this point.
+
+            for _i in 0..iters {
+                
+                let kwargs = vec![0u8, 0u8, 0u8, 0u8];
+                let prebase = BigUint::from_str("23552530042895608691117491600237253503421571604130267549958491021862557968545686650800025417751442581478174883424904776631876311458942118138893331901743169597661103773770501396751931180680461715319909954058769027194390973562854582264459370466291468019765154132647504506261367634054380070159275701379300307272335771799914459787807012495205782256505101544446266761033256146121950939307836719442112831034994479582235844733572993896880355404750973902938047876522277943510414178534887384633289423913424130519568015741231524628797203316030476541265585566130668519397546518664878566691246015353683371189494506737035298376029").unwrap();
+                //println!("rsa encrypt base is : {:?}", base.to_string());
+                let base = BigUint::from_bytes_be(&kwargs)+prebase;
+                let modulus = BigUint::from_str("23634079817185953672976156667310480569413861571731128342835120902304725194227604734714864761086282226459451070158067246952841325486932164846983400935771070301126897411714395503603071382206614188196926295315183654131292472566017685773629645844056754485027258134445867043542500090829553127704850640662457344972692669924565578992582545398236097996200954885187629998219401740720070599068905420186439289579209007101333509176240442477928207938033819915248563356506104114504039121967188248884349066675777802745530105385590778422679336258192770716244023175746869401116092703835973750824729518784331012883475852216925149145477").unwrap();
+                //println!("rsa encrypt modulus is : {:?}", modulus.to_string());
+                let exponent= BigUint::from_str("65537").unwrap();
+                //println!("rsa encrypt exponent is : {:?}", exponent.to_string());
+
+                let start = std::time::Instant::now();
+
+                let result = base.modpow(&exponent, &modulus);
+        
+                total_duration += start.elapsed();
+
+                //println!("rsa encrypt result is : {:?}", result.to_bytes_be());
+                //println!("rsa encrypt result is : {:?}", result);
+                
+            }
+            total_duration
+
+            // println!("ress: {:?}", hex::encode(ress));
+        })
+    });
+}
+
 criterion_group!(
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
@@ -819,6 +922,8 @@ criterion_group!(
         bench_poseidon,
         bench_vergrth16,
         bench_wasmadd,
-        bench_wasmtls
+        bench_wasmtls,
+        bench_wasmcalc,
+        bench_rsamod
 );
 criterion_main!(benches);
