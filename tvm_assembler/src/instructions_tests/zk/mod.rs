21f6395599d7874b741c414ad3c3f3db79f55ab8
alinaT <alina.t@gosh.sh>
2025-07-31 00:05:37 +0300

    remove all separate providers tests

--- tvm_assembler/src/instructions_tests/zk/mod.rs
+++ tvm_assembler/src/instructions_tests/zk/mod.rs
@@ -2,6 +2,4 @@ pub mod test_helper;
 pub mod test_poseidon_bad_args;
 pub mod test_zk_bad_proof;
 pub mod test_zk_bad_public_input;
-pub mod test_zk_for_karrier_one;
-pub mod test_zk_for_microsoft;
-pub mod test_zk_for_slack;
+
