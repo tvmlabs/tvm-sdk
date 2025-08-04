66bcd98f05dc727d9d00eae7edc9f0e94d088e8a
ignaty8 <ignaty8@users.noreply.github.com>
2025-08-04 10:02:24 +0200

    Feat/wasm io libs (#120)

--- tvm_executor/src/transaction_executor.rs
+++ tvm_executor/src/transaction_executor.rs
@@ -11,6 +11,8 @@
 #![allow(clippy::too_many_arguments)]
 
 use std::cmp::min;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::LinkedList;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -136,7 +138,9 @@ pub struct ExecuteParams {
     pub termination_deadline: Option<Instant>,
     pub execution_timeout: Option<Duration>,
     pub wasm_binary_root_path: String,
-    pub wasm_binary_hash_whitelist: String,
+    pub wasm_hash_whitelist: HashSet<[u8; 32]>,
+    pub wasm_engine: Option<wasmtime::Engine>,
+    pub wasm_component_cache: HashMap<[u8; 32], wasmtime::component::Component>,
 }
 
 pub struct ActionPhaseResult {
@@ -182,7 +186,9 @@ impl Default for ExecuteParams {
             termination_deadline: None,
             execution_timeout: None,
             wasm_binary_root_path: "./config/wasm".to_owned(),
-            wasm_binary_hash_whitelist: "".to_owned(),
+            wasm_hash_whitelist: HashSet::new(),
+            wasm_engine: None,
+            wasm_component_cache: HashMap::new(),
         }
     }
 }
@@ -562,6 +568,9 @@ pub trait TransactionExecutor {
             params.block_collation_was_finished.clone(),
         )
         .set_wasm_root_path(params.wasm_binary_root_path.clone())
+        .set_wasm_hash_whitelist(params.wasm_hash_whitelist.clone())
+        .extern_insert_wasm_engine(params.wasm_engine.clone())
+        .extern_insert_wasm_component_cache(params.wasm_component_cache.clone())
         .create();
 
         if let Some(modifiers) = params.behavior_modifiers.clone() {
