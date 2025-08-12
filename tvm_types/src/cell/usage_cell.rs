38dbec5d1823e859c6466aec4b7ec8a445778582
Michael Vlasov <melsomino@gmail.com>
2025-08-12 19:17:45 +0300

    wip

--- tvm_types/src/cell/usage_cell.rs
+++ tvm_types/src/cell/usage_cell.rs
@@ -57,11 +57,6 @@ impl UsageCell {
             }
             self.visited.count.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
             map.insert(self.cell.repr_hash(), Cell::with_usage(self.clone()));
-            if map.len() > 1000 {
-                println!("{}", std::backtrace::Backtrace::force_capture());
-            } else if map.len() > 2000 {
-                panic!("Nested usage cells can not cause stack overflow");
-            }
             true
         } else {
             false
