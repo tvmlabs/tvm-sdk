fbc2a19d277879b2df5419d6e8a7de091922a83d
alinaT <alina.t@gosh.sh>
2025-09-14 07:01:14 +0300

    update ExecuteParams

--- tvm_executor/src/transaction_executor.rs
+++ tvm_executor/src/transaction_executor.rs
@@ -111,6 +111,8 @@ const MAX_MSG_CELLS: usize = 1 << 13;
 
 const STORAGE_FEE_COOLER_TIME: u32 = 2592000;
 
+pub const DEFAULT_VERGRTH16_VERIFICATION_KEY_SERIALIZED: [u8; 296] = [226, 242, 109, 190, 162, 153, 245, 34, 59, 100, 108, 177, 251, 51, 234, 219, 5, 157, 148, 7, 85, 157, 116, 65, 223, 217, 2, 227, 167, 154, 77, 45, 171, 183, 61, 193, 127, 188, 19, 2, 30, 36, 113, 224, 192, 139, 214, 125, 132, 1, 245, 43, 115, 214, 208, 116, 131, 121, 76, 173, 71, 120, 24, 14, 12, 6, 243, 59, 188, 76, 121, 169, 202, 222, 242, 83, 166, 128, 132, 211, 130, 241, 119, 136, 248, 133, 201, 175, 209, 118, 247, 203, 47, 3, 103, 137, 237, 246, 146, 217, 92, 189, 222, 70, 221, 218, 94, 247, 212, 34, 67, 103, 121, 68, 92, 94, 102, 0, 106, 66, 118, 30, 31, 18, 239, 222, 0, 24, 194, 18, 243, 174, 183, 133, 228, 151, 18, 231, 169, 53, 51, 73, 170, 241, 37, 93, 251, 49, 183, 191, 96, 114, 58, 72, 13, 146, 147, 147, 142, 25, 237, 246, 146, 217, 92, 189, 222, 70, 221, 218, 94, 247, 212, 34, 67, 103, 121, 68, 92, 94, 102, 0, 106, 66, 118, 30, 31, 18, 239, 222, 0, 24, 194, 18, 243, 174, 183, 133, 228, 151, 18, 231, 169, 53, 51, 73, 170, 241, 37, 93, 251, 49, 183, 191, 96, 114, 58, 72, 13, 146, 147, 147, 142, 25, 2, 0, 0, 0, 0, 0, 0, 0, 188, 109, 65, 14, 59, 194, 107, 53, 43, 136, 71, 184, 217, 252, 205, 146, 137, 248, 166, 82, 243, 30, 4, 205, 71, 203, 158, 80, 49, 134, 196, 45, 104, 33, 147, 62, 5, 214, 248, 224, 214, 11, 163, 236, 65, 113, 21, 154, 124, 161, 149, 238, 58, 248, 236, 80, 209, 30, 86, 217, 167, 170, 27, 129];
+
 #[derive(Eq, PartialEq, Debug)]
 pub enum IncorrectCheckRewrite {
     Anycast,
@@ -136,6 +138,7 @@ pub struct ExecuteParams {
     pub available_credit: i128,
     pub termination_deadline: Option<Instant>,
     pub execution_timeout: Option<Duration>,
+    pub vergrth16_verififcation_key_serialized: [u8; 296],
     pub wasm_binary_root_path: String,
     pub wasm_hash_whitelist: HashSet<[u8; 32]>,
     pub wasm_engine: Option<wasmtime::Engine>,
@@ -183,10 +186,12 @@ impl Default for ExecuteParams {
             available_credit: 0,
             termination_deadline: None,
             execution_timeout: None,
+            vergrth16_verififcation_key_serialized: DEFAULT_VERGRTH16_VERIFICATION_KEY_SERIALIZED,
             wasm_binary_root_path: "./config/wasm".to_owned(),
             wasm_hash_whitelist: HashSet::new(),
             wasm_engine: None,
             wasm_component_cache: HashMap::new(),
+
         }
     }
 }
@@ -533,6 +538,7 @@ pub trait TransactionExecutor {
         .set_engine_available_credit(params.available_credit)
         .set_wasm_hash_whitelist(params.wasm_hash_whitelist.clone())
         .set_wasm_block_time(params.block_unixtime.into())
+        .set_vergrth16_verififcation_key_serialized(params.vergrth16_verififcation_key_serialized)
         .extern_insert_wasm_engine(params.wasm_engine.clone())
         .extern_insert_wasm_component_cache(params.wasm_component_cache.clone())
         .create();
