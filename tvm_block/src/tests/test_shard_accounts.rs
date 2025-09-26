bb2a41fd67641cd1fb689d8b2270678bd2fd28e8
Michael Vlasov <melsomino@gmail.com>
2025-09-26 18:01:06 +0300

    wip

--- tvm_block/src/tests/test_shard_accounts.rs
+++ tvm_block/src/tests/test_shard_accounts.rs
@@ -10,9 +10,14 @@
 // limitations under the License.
 
 use super::*;
-use crate::AccountCellStruct;
+use crate::{define_HashmapE, AccountCellStruct};
 use crate::generate_test_account_by_init_code_hash;
 use crate::write_read_and_assert;
+use blake3::Hasher;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Instant;
+use tvm_types::HashmapE;
 
 #[test]
 fn test_serialization_shard_account() {
@@ -74,4 +79,51 @@ fn test_external_account_serialization() {
 
     let serialized_full = shard_acc.serialize().unwrap();
     assert_eq!(serialized_full.repr_hash(), serialized_unloaded.repr_hash());
+}
+
+define_HashmapE!(ShardAccounts2, 256, ShardAccount);
+
+#[test]
+fn test_shard_account_insert_time() {
+    let mut accounts = HashMap::new();
+    let time = Instant::now();
+    for n in 5..100_000u32 {
+        let account_id = UInt256::from(n.to_be_bytes().as_slice());
+        let acc = generate_test_account_by_init_code_hash(false);
+        accounts.insert(
+            account_id.clone(),
+            ShardAccount::with_params(&acc, UInt256::default(), 0, Some(Default::default()))
+                .unwrap(),
+        );
+    }
+    println!("Generation time {:?}", time.elapsed());
+
+    let mut shard_accounts = ShardAccounts::default();
+    let time = Instant::now();
+    for (id, shard_acc) in &accounts {
+        shard_accounts.insert(&id, &shard_acc).unwrap();
+    }
+    println!("Shard accounts insertion time {:?}", time.elapsed());
+
+    let mut shard_accounts2 = ShardAccounts2::default();
+    let time = Instant::now();
+    for (id, shard_acc) in &accounts {
+        shard_accounts2.set(id, &shard_acc).unwrap();
+    }
+    println!("Shard accounts 2 insertion time {:?}", time.elapsed());
+
+    let subset =
+        accounts.iter().map(|(k, v)| (k.clone(), v.clone())).take(300).collect::<Vec<_>>();
+
+    let time = Instant::now();
+    for (id, shard_acc) in &subset {
+        shard_accounts.insert(id, shard_acc).unwrap();
+    }
+    println!("Shard accounts reinsertion time {:?}", time.elapsed());
+
+    let time = Instant::now();
+    for (id, shard_acc) in &subset {
+        shard_accounts2.set(id, shard_acc).unwrap();
+    }
+    println!("Shard accounts 2 reinsertion time {:?}", time.elapsed());
 }
