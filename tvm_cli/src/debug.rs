124c4e5187c7a79f50c6e68278272c5474111401
sehor05 <serhii.h@gosh.sh>
2025-10-09 16:41:26 +0300

    fix client and cli

--- tvm_cli/src/debug.rs
+++ tvm_cli/src/debug.rs
@@ -1245,8 +1245,7 @@ pub async fn execute_debug(
         trace_callback: Some(generate_callback(matches, tonos_config)),
         ..ExecuteParams::default()
     };
-
-    let tr = executor.execute_with_libs_and_params(message, account_root, params).map_err(|e| {
+    let tr = executor.execute_with_libs_and_params(message.cloned().as_mut(), account_root, params).map_err(|e| {
         let exit_code = match e.downcast_ref() {
             Some(tvm_executor::ExecutorError::NoAcceptError(exit_code, _)) => *exit_code,
             Some(tvm_executor::ExecutorError::TvmExceptionCode(exit_code)) => *exit_code as i32,
