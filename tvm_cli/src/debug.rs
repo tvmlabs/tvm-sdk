523ba0c0982a47e57c6995dbdc436a206eb6f196
sehor05 <serhii.h@gosh.sh>
2025-10-23 00:22:50 +0300

    fmt

--- tvm_cli/src/debug.rs
+++ tvm_cli/src/debug.rs
@@ -1245,21 +1245,19 @@ pub async fn execute_debug(
         trace_callback: Some(generate_callback(matches, tonos_config)),
         ..ExecuteParams::default()
     };
-    let tr = executor
-        .execute_with_libs_and_params(message, account_root, params)
-        .map_err(|e| {
-            let exit_code = match e.downcast_ref() {
-                Some(tvm_executor::ExecutorError::NoAcceptError(exit_code, _)) => *exit_code,
-                Some(tvm_executor::ExecutorError::TvmExceptionCode(exit_code)) => *exit_code as i32,
-                None => tvm_vm::error::tvm_exception_or_custom_code(&e),
-                _ => return format!("Debug failed: {}", e),
-            };
-            let result = json!({
-                "exit_code": exit_code,
-                "message": e.to_string(),
-            });
-            format!("{:#}", result)
+    let tr = executor.execute_with_libs_and_params(message, account_root, params).map_err(|e| {
+        let exit_code = match e.downcast_ref() {
+            Some(tvm_executor::ExecutorError::NoAcceptError(exit_code, _)) => *exit_code,
+            Some(tvm_executor::ExecutorError::TvmExceptionCode(exit_code)) => *exit_code as i32,
+            None => tvm_vm::error::tvm_exception_or_custom_code(&e),
+            _ => return format!("Debug failed: {}", e),
+        };
+        let result = json!({
+            "exit_code": exit_code,
+            "message": e.to_string(),
         });
+        format!("{:#}", result)
+    });
     match tr {
         Ok(data) => Ok(data.0),
         Err(e) => Err(e),
