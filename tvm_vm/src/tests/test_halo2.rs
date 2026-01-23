b8065f4b36379323c2b4638237e8e7db00e2e2bc
alinaT <alina.t@gosh.sh>
2026-01-23 15:22:43 +0300

    negative

--- tvm_vm/src/tests/test_halo2.rs
+++ tvm_vm/src/tests/test_halo2.rs
@@ -98,3 +98,76 @@ fn test() {
     println!("res: {:?}", res);
     assert!(res == true);
 }
+
+#[test]
+fn test_negative() {
+    let elector_code = load_boc("benches/elector-code.boc");
+    let elector_data = load_boc("benches/elector-data.boc");
+    let config_data = load_boc("benches/config-data.boc");
+    let mut ctrls = SaveList::default();
+    ctrls.put(4, &mut StackItem::Cell(elector_data)).unwrap();
+    let params = vec![
+        StackItem::int(0x76ef1ea),
+        StackItem::int(0),
+        StackItem::int(0),
+        StackItem::int(1633458077),
+        StackItem::int(0),
+        StackItem::int(0),
+        StackItem::int(0),
+        StackItem::tuple(vec![StackItem::int(1000000000), StackItem::None]),
+        StackItem::slice(
+            SliceData::from_string(
+                "9fe0000000000000000000000000000000000000000000000000000000000000001_",
+            )
+            .unwrap(),
+        ),
+        StackItem::cell(config_data.reference(0).unwrap()),
+        StackItem::None,
+        StackItem::int(0),
+    ];
+    ctrls.put(7, &mut StackItem::tuple(vec![StackItem::tuple(params)])).unwrap();
+    let stack = Stack::new();
+    let mut engine = Engine::with_capabilities(DEFAULT_CAPABILITIES).setup_with_libraries(
+        SliceData::load_cell_ref(&elector_code).unwrap(),
+        Some(ctrls.clone()),
+        Some(stack.clone()),
+        None,
+        vec![],
+    );
+
+    let sk_u: u64 = 23;
+    let token_type: u64 = 1;
+    let private_note_sum: u64 = 1000;
+
+    let sk_u_ = Fr::from(sk_u);
+    let token_type_ = Fr::from(token_type);
+    let private_note_sum_ = Fr::from(private_note_sum);
+ 
+    let sk_u_commitment = poseidon_hash([sk_u_, Fr::zero()]);
+    let data_to_hash = [sk_u_commitment, private_note_sum_, token_type_, sk_u_];
+    let digest = poseidon_hash(data_to_hash);
+    let digest: [u8; 32] = digest.to_bytes();
+
+    let token_type_wrong: u64 = 2;
+    engine.cc.stack.push(StackItem::integer(IntegerData::from_unsigned_bytes_be(&digest.clone())));
+    engine.cc.stack.push(StackItem::int(token_type_wrong));
+    engine.cc.stack.push(StackItem::int(private_note_sum));
+
+    let params = read_kzg_params("kzg_params.bin".to_string());
+    let mut pub_inputs = vec![private_note_sum_, token_type_, Fr::from_bytes(&digest).unwrap()];
+    let proof = generate_proof(&params, Some(token_type_), Some(private_note_sum_), Some(sk_u_), Some(sk_u_commitment),  &mut pub_inputs);
+
+    
+    let proof_cell = pack_data_to_cell(&proof.clone(), &mut 0).unwrap();
+    engine.cc.stack.push(StackItem::cell(proof_cell.clone()));
+
+    let start: Instant = Instant::now();
+    let _ = execute_halo2_proof_verification(&mut engine).unwrap();
+    let elapsed = start.elapsed().as_micros();
+
+    println!("elapsed in microsecond: {:?}", elapsed);
+
+    let res = engine.cc.stack.get(0).as_bool().unwrap();
+    println!("res: {:?}", res);
+    assert!(res == false);
+}
