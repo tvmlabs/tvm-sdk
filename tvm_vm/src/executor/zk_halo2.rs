bda450b61d517c847a2f0fd611a4f16c045f1e9a
alinaT <alina.t@gosh.sh>
2026-01-24 10:55:11 +0300

    test

--- tvm_vm/src/executor/zk_halo2.rs
+++ tvm_vm/src/executor/zk_halo2.rs
@@ -80,10 +80,10 @@ pub(crate) fn execute_halo2_proof_verification(engine: &mut Engine) -> Status {
 
     let token_type = Fr::from(token_type);
     let private_note_sum = Fr::from(private_note_sum);
-  /*let private_note_digest = Fr::from_bytes(private_note_digest_bytes).unwrap();
+    let private_note_digest = Fr::from_bytes(private_note_digest_bytes).unwrap();
 
 
-    println!("private_note_digest: {:?}", private_note_digest);
+   /* println!("private_note_digest: {:?}", private_note_digest);
     println!("token_type: {:?}", token_type);
     println!("private_note_sum: {:?}", private_note_sum);
 
@@ -97,7 +97,7 @@ pub(crate) fn execute_halo2_proof_verification(engine: &mut Engine) -> Status {
    
     let res = verify_proof_(&params, &proof, &vk, pub_inputs);*/
 
-    let res =  boolean!(true);
+    let res =  boolean!(false);
     engine.cc.stack.push(res);
 
     Ok(())
