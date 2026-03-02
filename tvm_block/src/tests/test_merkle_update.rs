79bb4b96770f26b8bf91614ca43b172206e3afc4
Alexander <alexander.s@gosh.sh>
2026-03-02 20:41:11 +0300

    Fix test files

--- tvm_block/src/tests/test_merkle_update.rs
+++ tvm_block/src/tests/test_merkle_update.rs
@@ -1321,8 +1321,8 @@ fn test_account_merkle_update() -> anyhow::Result<()> {
     rlimit::setrlimit(rlimit::Resource::AS, 512 * 1024 * 1024, 512 * 1024 * 1024)
         .expect("Failed to set memory limit");
 
-    let old_cell = Cell::construct_from_file("/home/user/GOSH/acki-nacki/test_accounts/b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68:b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68_old").expect("Failed to load cell from file");
-    let new_cell = Cell::construct_from_file("/home/user/GOSH/acki-nacki/test_accounts/b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68:b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68_new").expect("Failed to load cell from file");
+    let old_cell = Cell::construct_from_file("src/tests/data/test_accounts/b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68:b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68_old").expect("Failed to load cell from file");
+    let new_cell = Cell::construct_from_file("src/tests/data/test_accounts/b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68:b48dc01a6674e616f1f7c4c5e7212af4227b95c3111e1c2051850c35425f7d68_new").expect("Failed to load cell from file");
     let merkle_update = crate::MerkleUpdate::create(&old_cell, &new_cell).ok();
     assert!(merkle_update.is_some());
     Ok(())
