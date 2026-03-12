ab29a64b8310cad008ce6aa3e758c4699208a519
roman.ec4a <roman.ec4a@gmail.com>
2026-03-12 18:46:04 +0300

    Fix/wasm-web build and ZK dependency compatibility
    The `halo2-base` and `pse-poseidon` crates are currently incompatible with WASM targets. This change makes these dependencies optional, gates ZK-related functions behind target architecture checks, and configures `wasm-opt` to support bulk-memory and non-trapping float-to-int operations for web builds.

--- tvm_vm/src/executor/zk_stuff/bn254/poseidon/mod.rs
+++ tvm_vm/src/executor/zk_stuff/bn254/poseidon/mod.rs
@@ -5,10 +5,13 @@ use ark_ff::BigInteger;
 use ark_ff::PrimeField;
 use byte_slice_cast::AsByteSlice;
 use ff::PrimeField as OtherPrimeField;
+#[cfg(not(target_arch = "wasm32"))]
 use halo2_base::halo2_proofs::halo2curves::bn256::Fr as halo2_Fr;
+#[cfg(not(target_arch = "wasm32"))]
 use halo2_base::utils::ScalarField;
 use neptune::Poseidon;
 use neptune::poseidon::HashMode::OptimizedStatic;
+#[cfg(not(target_arch = "wasm32"))]
 use pse_poseidon::Poseidon as pse_poseidon;
 
 use crate::executor::zk_stuff::FrRepr;
@@ -136,6 +139,7 @@ const RATE: usize = 2;
 const R_F: usize = 8;
 const R_P: usize = 57;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub fn poseidon_bytes_axiom(
     inputs: &Vec<Vec<u8>>,
 ) -> Result<[u8; FIELD_ELEMENT_SIZE_IN_BYTES], ZkCryptoError> {
@@ -153,6 +157,13 @@ pub fn poseidon_bytes_axiom(
         output_as_field_element.to_bytes_le().try_into().unwrap();
 
     Ok(output)
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn poseidon_bytes_axiom(
+    _inputs: &Vec<Vec<u8>>,
+) -> Result<[u8; FIELD_ELEMENT_SIZE_IN_BYTES], ZkCryptoError> {
+    Err(InvalidInput)
 }
 
 pub fn poseidon_bytes_flat(
@@ -250,6 +261,7 @@ fn test_poseidon_bytes_flat() {
     assert_eq!(digest, etalon_res);
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 #[test]
 fn test_fr() {
     let input = vec![0xFF; 31];
