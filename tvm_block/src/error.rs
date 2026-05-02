5f38866a39ca86313593a63da3c1f7e9cb9b8842
Michael Vlasov <melsomino@gmail.com>
2026-05-02 18:04:09 +0300

    fixed thiserror formats

--- tvm_block/src/error.rs
+++ tvm_block/src/error.rs
@@ -13,40 +13,40 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum BlockError {
     /// Fatal error.
-    #[error("Fatal error: {}", 0)]
+    #[error("Fatal error: {0}")]
     FatalError(String),
     /// Invalid argument.
-    #[error("Invalid argument: {}", 0)]
+    #[error("Invalid argument: {0}")]
     InvalidArg(String),
     /// Invalid TL-B constructor tag.
     #[error("Invalid TL-B constructor tag `#{:x}` while parsing `{}` struct", t, s)]
     InvalidConstructorTag { t: u32, s: String },
     /// Invalid data.
-    #[error("Invalid data: {}", 0)]
+    #[error("Invalid data: {0}")]
     InvalidData(String),
     /// Invalid index.
-    #[error("Invalid index: {}", 0)]
+    #[error("Invalid index: {0}")]
     InvalidIndex(usize),
     /// Invalid operation.
-    #[error("Invalid operation: {}", 0)]
+    #[error("Invalid operation: {0}")]
     InvalidOperation(String),
     /// Item is not found.
-    #[error("{} is not found", 0)]
+    #[error("{0} is not found")]
     NotFound(String),
     /// Other error.
-    #[error("{}", 0)]
+    #[error("{0}")]
     Other(String),
     /// Attempting to read data from pruned branch cell.
-    #[error("Attempting to read {} from pruned branch cell", 0)]
+    #[error("Attempting to read {0} from pruned branch cell")]
     PrunedCellAccess(String),
     /// Wrong hash.
     #[error("Wrong hash")]
     WrongHash,
     /// Wrong merkle proof.
-    #[error("Wrong merkle proof: {}", 0)]
+    #[error("Wrong merkle proof: {0}")]
     WrongMerkleProof(String),
     /// Wrong merkle update.
-    #[error("Wrong merkle update: {}", 0)]
+    #[error("Wrong merkle update: {0}")]
     WrongMerkleUpdate(String),
     #[error("Bad signature")]
     BadSignature,
